9af33585a0adef36bcace654151f157654422305
Ed Pfromer <epfromer@gmail.com>
2020-08-28 08:52:53 -0600

    update

--- packages/mongodb-server/src/getWordCloud.ts
+++ packages/mongodb-server/src/getWordCloud.ts
@@ -1,11 +1,11 @@
-import { contactCollection } from '@klonzo/common'
+import { wordCloudCollection } from '@klonzo/common'
 import { Request, Response } from 'express'
 import { db } from './index'
 
 // HTTP GET /wordcloud
 export async function getWordCloud(req: Request, res: Response): Promise<void> {
   try {
-    const wordCloud = await db.collection(contactCollection).find().toArray()
+    const wordCloud = await db.collection(wordCloudCollection).find().toArray()
     res.json(wordCloud)
   } catch (err) {
     console.error(err.stack)

eb59eb88031b525afaa89a5b97fc8b306682689b
Ed Pfromer <epfromer@gmail.com>
2020-09-12 13:21:47 -0600

    update

--- packages/mysql-pst/src/index.ts
+++ packages/mysql-pst/src/index.ts
@@ -16,40 +16,63 @@ import {
 import * as dotenv from 'dotenv'
 import { v4 as uuidv4 } from 'uuid'
 
-dotenv.config()
-
-// http://localhost:8983/solr/#/
-// https://lucene.apache.org/solr/guide/
-
-// const insertEmails = async (emails: Email[]): Promise<void> => {}
-
-// const insertWordCloud = async (wordCloud: WordCloudTag[]): Promise<void> => {}
-
-// const insertEmailSentByDay = async (
-//   emailSentByDay: EmailSentByDay[]
-// ): Promise<void> => {}
-
-// const insertCustodians = async (custodians: Custodian[]): Promise<void> => {}
-
 async function run() {
+  dotenv.config()
+
+  console.log(`connect to mysql`)
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const mysql = require('mysql2')
+  const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+  })
+
+  connection.query('SELECT 1 + 1 AS solution', function (
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error
+    console.log('The solution is: ', results[0].solution)
+  })
+
+  // console.log(`connect to mysql`)
+  // console.log(process.env.MYSQL_HOST, process.env.MYSQL_ROOT_PASSWORD)
+
+  // // eslint-disable-next-line @typescript-eslint/no-var-requires
+  // const db = require('knex')({
+  //   client: 'mysql',
+  //   connection: {
+  //     host: '127.0.0.1',
+  //     user: 'foo',
+  //     password: 'f00bar',
+  //     database: dbName,
+  //   },
+  // })
+
+  // console.log('connected')
+
+  // const users = await db.select('now()')
+  // console.log(users)
+
+  // const insertEmails = async (emails: Email[]): Promise<void> => {}
+
+  // const insertWordCloud = async (wordCloud: WordCloudTag[]): Promise<void> => {}
+
+  // const insertEmailSentByDay = async (
+  //   emailSentByDay: EmailSentByDay[]
+  // ): Promise<void> => {}
+
+  // const insertCustodians = async (custodians: Custodian[]): Promise<void> => {}
+
   // console.log(`drop database`)
   // try {
   // } catch (err) {
   //   console.error(err)
   // }
 
-  console.log(`create database`)
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const solr = require('solr-client')
-  const client = solr.createClient()
-  client.basicAuth('admin', 'passtest')
-  client.add({ id: 12, title_t: 'Hello' }, function (err, obj) {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log('Solr response:', obj)
-    }
-  })
+  // console.log(`create database`)
 
   // console.log(`insert emails`)
   // const numEmails = await walkFSfolder(insertEmails)
